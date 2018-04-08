fd3e02588911e478c73d1cab07765d1543800c18
jawauntb <Juan@Jawauns-MBP.home>
2018-04-07 22:25:49 -0400

    made space for show usage and exit

--- Towers.java
+++ Towers.java
@@ -1,1 +1,22 @@
-public class Towers{}
+public class Towers{
+	/* main class where action will happen */
+
+	public static void main (String [] args) {
+	
+	//if we give the wrong number of args then show usage and exit
+
+	if (args.length != 1) {
+		showUsageAndExit();
+	}
+	
+	int rings=0;
+	try{
+		}catch(){}
+	
+	
+	
+	}
+
+
+
+}
