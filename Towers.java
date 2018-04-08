92bb2e31494dfe58c5d38148b9bd83ec68c2b6c3
jawauntb <Juan@Jawauns-MBP.home>
2018-04-08 14:00:36 -0400

    print methods

--- Towers.java
+++ Towers.java
@@ -23,6 +23,51 @@ public class Towers{
 	solve(towers); 
 	} // main()
 
+    // =========================================================================
+    public static void print (int[][] towers) {
+
+	int rings = towers[0].length;
+	for (int row = rings - 1; row >= 0; row -= 1) {
+	    for (int tower = 0; tower < 3; tower += 1) {
+		printRing(towers[tower][row], rings);
+		System.out.print("\t");
+	    }
+	    System.out.println();
+	}
+	System.out.println();
+
+    }
+    // =========================================================================
+
+
+
+    // =========================================================================
+    public static void printRing (int size, int rings) {
+
+	int spaces = rings - size;
+	printChar(' ', spaces);
+	printChar('=', size);
+	printChar('|', 1);
+	printChar('=', size);
+	printChar(' ', spaces);
+
+    }
+    // =========================================================================
+
+
+
+    // =========================================================================
+    	public static void printChar (char c, int reps) {
+
+		for (int i = 0; i < reps; i += 1) {
+		    System.out.print(c);
+		}
+
+    }
+    // =========================================================================
+
+
+
 	public static void showUsageAndExit () {
         System.err.println("Usage: type in your command line the following: java Towers <the num of rings>");
 	System.exit(1); 
