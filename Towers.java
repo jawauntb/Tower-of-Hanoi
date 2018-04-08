f516253bc363a0356ec03d100480d6ff06eb5c68
jawauntb <Juan@Jawauns-MBP.home>
2018-04-08 13:56:58 -0400

    added main method to fit real methods

--- Towers.java
+++ Towers.java
@@ -8,15 +8,25 @@ public class Towers{
 	if (args.length != 1) {
 		showUsageAndExit();
 	}
-	
-	int rings=0;
-	try{
-		}catch(){}
-	
-	
-	
+	// Grab the argument.
+	int rings = 0;
+	try {
+	    rings = Integer.parseInt(args[0]);
+	} catch (NumberFormatException e) {
+	    showUsageAndExit();
+	}
+	if (rings < 0) {
+	    showUsageAndExit();
 	}
 
+	int[][] towers = initialize(rings);
+	solve(towers); 
+	} // main()
+
+	public static void showUsageAndExit () {
+        System.err.println("Usage: type in your command line the following: java Towers <the num of rings>");
+	System.exit(1); 
+	}
 
 
 }
